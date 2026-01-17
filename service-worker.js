ccc2f94bbc474ba686dff8c8f5828eccbbada3db
dummigui9-siva <dummigui9@gmail.com>
2026-01-17 13:24:30 +0530

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,6 @@
 const CACHE_NAME = "expense-tracker-v1";
-const FILES_TO_CACHE = [
+
+const ASSETS = [
   "./",
   "./index.html",
   "./manifest.json"
@@ -7,9 +8,20 @@ const FILES_TO_CACHE = [
 
 self.addEventListener("install", event => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(FILES_TO_CACHE))
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
   );
   self.skipWaiting();
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
+      )
+    )
+  );
+  self.clients.claim();
 });
 
 self.addEventListener("fetch", event => {
